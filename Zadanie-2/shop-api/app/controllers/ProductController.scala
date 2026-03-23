5c34277a3f97e38be79126eed220b55e67cc6e02
Jan Kwinta <jankwinta@icloud.com>
2026-03-23 14:34:53 +0100

    C.2.x.x.23.03 - zmiany w zarzadzaniu kategoriami

--- Zadanie-2/shop-api/app/controllers/ProductController.scala
+++ Zadanie-2/shop-api/app/controllers/ProductController.scala
@@ -11,8 +11,8 @@ class ProductController @Inject()(val controllerComponents: ControllerComponents
   
   // lista jako baza danych
   private val productsList = ListBuffer(
-    Product(1, "GeForce RTX 5080", 6000.0),
-    Product(2, "Logitech MX Master 3S", 450.0)
+    Product(1, "GeForce RTX 5080", 6000.0, 1),
+    Product(2, "Logitech MX Master 3S", 450.0, 2)
   )
   
   // 1. Show all (GET)
--- Zadanie-2/shop-api/app/models/Product.scala
+++ Zadanie-2/shop-api/app/models/Product.scala
@@ -2,7 +2,7 @@ package models
 
 import play.api.libs.json._
 
-case class Product(id: Long, name: String, price: Double)
+case class Product(id: Long, name: String, price: Double, categoryId: Long)
 
 object Product {
   implicit val format: OFormat[Product] = Json.format[Product]
