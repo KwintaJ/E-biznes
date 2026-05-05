806b1bcc952e630c23868883305bbff98ed770ad
Jan Kwinta <jankwinta@icloud.com>
2026-05-05 15:53:42 +0200

    xx.05.05 - debugging

--- Zadanie-5/frontend/src/api/axios.js
+++ Zadanie-5/frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://bs-local.com:8080/products',
   headers: {
     'Content-Type': 'application/json',
   }
